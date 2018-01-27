5e37b361ecbe73946c7ac8ad7964ae5ca7dda0c8
Matt Chapman <chapabu101@gmail.com>
2018-01-27 02:13:47 +0000

    Fixed fonts task

--- STARTERKIT/tasks/fonts.js
+++ STARTERKIT/tasks/fonts.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 
 const fonts = function () {
-  gulp.src('./assets/src/fonts/**/*.{eot,ttf,woff,woff2,otf,svg}')
-    .dest('./assets/dist');
+  return gulp.src('./assets/src/fonts/**/*.{eot,ttf,woff,woff2,otf,svg}')
+    .pipe(gulp.dest('./assets/dist'));
 };
 
 fonts.displayName = 'fonts';
